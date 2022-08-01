42c4de8ba0b0588fe17c6117640698bd75d884ca
Glen Souza <glenster75@gmail.com>
2022-08-01 14:26:18 -0700

    set up service bus info

---
+++ NodeJS-Typescript/src/serviceBusInfo.ts
@@ -0,0 +1,7 @@
+export class ServiceBusInfo {
+    public TenantId: string = '<YOUR_TENANT_ID>';
+    public ClientId: string = '<YOUR_CLIENT_ID>';
+    public ClientSecret: string = '<YOUR_CLIENT_SECRET>';
+    public Endpoint: string = '<SERVICE_BUS_ENDPOINT>';
+    public QueueName: string = '<QUEUE_NAME>';
+}
